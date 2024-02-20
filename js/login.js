5442f15642a631a1ddc957d7206c23247038172e
h220101 <ssw3417@naver.com>
2024-02-20 15:28:00 +0900

    Add: login 조건 수정

--- js/login.js
+++ js/login.js
@@ -10,7 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
     let storedUserData = JSON.parse(localStorage.getItem('userData'));
 
     if (storedUserData && id === storedUserData.id && password === storedUserData.password) {
-      window.location.href = './settings.html';
+      let currentLetters = localStorage.getItem('currentLetters');
+
+      if (currentLetters === null || parseInt(currentLetters) === 0){
+        window.location.href = './settings.html';
+      } else {
+        window.location.href = './main.html';
+      }
     } else {
       alert('아이디 또는 비밀번호가 잘못되었습니다. 다시 입력해주세요.');
     }
