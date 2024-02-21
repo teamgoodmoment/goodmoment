9cc406c9b3330136bd0e070b0de87ac1ef8e667a
h220101 <ssw3417@naver.com>
2024-02-21 14:15:35 +0900

    login 유효성 검사 및 신규 계정 조건추가

--- js/login.js
+++ js/login.js
@@ -13,31 +13,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
     console.log(localStorage)
 
-    if (storedUserData && id === storedUserData.id && password === storedUserData.password) {
-      //쪽지 가져오기
-      //let currentLetters = localStorage.getItem('currentLetters');
+    if (storedUserData) {
+      //가입 한 적이 있는 경우
+      if (id === storedUserData.id && password === storedUserData.password) {
+        //로그인 성공 시 해당 유저로 세팅정보 Date, time 가져오기
+        let openingDate = localStorage.getItem(`${id}openingDate`);
+        let openingTime = localStorage.getItem(`${id}openingTime`);
 
-      //쪽지 여부 확인해서 페이지 이동
-      // if (currentLetters === null || parseInt(currentLetters) === 0){
-      //   window.location.href = './settings.html';
-      // } else {
-      //   window.location.href = './main.html;
-      // }
-
-      
-      //세팅 Date, time 가져오기
-      let openingDate = localStorage.getItem(`${id}openingDate`);
-      let openingTime = localStorage.getItem(`${id}openingTime`);
-
-      if (openingDate === null || openingTime === null) {
-          //세팅이 안 되어 있으면 세팅 페이지로 이동
+        if (openingDate === null || openingTime === null) {
+          //세팅을 안했으면 세팅 페이지로 이동
           window.location.href = './settings.html';
           return; 
+        } else {
+          //세팅이 되어있으면 메인 페이지로 이동
+          window.location.href = './main.html';
+        }
+      } else {
+        //유저 로그인 정보 검사
+        alert('아이디 또는 비밀번호가 잘못되었습니다. 다시 입력해주세요.');
       }
-      //세팅이 되어있으면 메인 페이지로 이동
-     window.location.href = './main.html';
     } else {
-      alert('아이디 또는 비밀번호가 잘못되었습니다. 다시 입력해주세요.');
+      //가입한 적이 없는 경우
+      if(confirm('가입하지 않은 계정입니다. 회원가입 페이지로 이동하시겠습니까?')) {
+        window.location.href = './register.html'
+      } else {
+        //유저가 취소한 경우
+      }
     }
   });
 });
